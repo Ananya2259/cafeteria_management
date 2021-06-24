35c6b121655688213337e3dc2263b86b4c4a777e
Ananya2259 <ananyagurumoorthy2002@gmail.com>
2021-06-24 18:57:45 +0530

    Create address model

---
+++ db/migrate/20210624132659_create_addresses.rb
@@ -0,0 +1,10 @@
+class CreateAddresses < ActiveRecord::Migration[6.1]
+  def change
+    create_table :addresses do |t|
+      t.bigint :user_id
+      t.string :address
+
+      t.timestamps
+    end
+  end
+end
